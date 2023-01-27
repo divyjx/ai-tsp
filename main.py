571b2e5fbf0dd264e85d4a8cabe4473cefec49cd
divyjx <divyjx@gmail.com>
2023-01-27 16:45:33 +0530

    #1 method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys 
-f=open(sys.argv[1],"r")
+# f=open(sys.argv[1],"r")
+f=open("euc_100","r")
 Cities=[]
 name=f.readline().rstrip("\n")
 number=int(f.readline().rstrip("\n"))
@@ -24,3 +25,27 @@ for i in range(number):
     disMat.append(x)
 nD=np.array(list(map(conv,disMat)))
 # print(nD,nC)
+
+# naive approach - minimum sussesive roots
+global visited
+visited=[]
+start=0
+startCorr=nC[0]
+cost=0
+curr=0
+def mini(arr):
+    m=100000
+    ind=0
+    for i in range(len(arr)):
+        if i in visited:
+            continue
+        if arr[i]<m:
+            m=arr[i]
+            ind=i
+    return m,i        
+while len(visited)!=100:
+    visited.append[curr]
+    x=mini(nD[curr])
+    cost+=mini[0]
+    curr=mini[1]
+print(curr,cost)
