686fa2dfa73917b4633f7d8e6f639268525ccf86
divyjx <divyjx@gmail.com>
2023-01-29 01:10:26 +0530

    fin

--- mst_chris.py
+++ mst_chris.py
@@ -10,7 +10,7 @@ import copy
 
 start = time.time()
 # f=open(sys.argv[1],"r")
-f = open("euc_100", "r")
+f = open("noneuc_100", "r")
 Cities = []
 name = f.readline().rstrip("\n")
 number = int(f.readline().rstrip("\n"))
@@ -147,7 +147,7 @@ g.EulerianTreeFinder()
 
 f=open("eu_matrix.txt","w")
 res = (g.EuMatrix)
-print(*res)
+# print(*res)
 for x in range(len(res)):
     for y in range(len(res)):
         res[y][x]=res[x][y]
@@ -166,82 +166,85 @@ for x in range(len(res)):
 # dfs caculation
 
 
-# adj = defaultdict(list)
-# vis = defaultdict(bool)
-# Euler = [0]*(2*g.vertices)
+adj = defaultdict(list)
+vis = defaultdict(bool)
+Euler = [0]*(2*g.vertices)
 
-# def add_edge(u, v, w):
-#     adj[u].append((v, w))
-#     adj[v].append((u, w))
+def add_edge(u, v, w):
+    adj[u].append((v, w))
+    adj[v].append((u, w))
 
-# def eulerTree(u, index):
-#     vis[u] = True
-#     Euler[index] = u
-#     index += 1
-#     for nbr in adj[u]:
-#         if not vis[nbr[0]]:
-#             index = eulerTree(nbr[0], index)
-#             Euler[index] = u
-#             index += 1
-#     return index
+def eulerTree(u, index):
+    vis[u] = True
+    Euler[index] = u
+    index += 1
+    for nbr in adj[u]:
+        if not vis[nbr[0]]:
+            index = eulerTree(nbr[0], index)
+            Euler[index] = u
+            index += 1
+    return index
 
-# def EulerTour(root, N):
-#     path = []
-#     index = 0
-#     eulerTree(root, index)
-#     for i in range(2*N-1):
-#         path.append(Euler[i])
-#     return path
+def EulerTour(root, N):
+    path = []
+    index = 0
+    eulerTree(root, index)
+    for i in range(2*N-1):
+        path.append(Euler[i])
+    return path
 
-# for x in g.EuTree:
-#     add_edge(x[0], x[1], x[2])
+for x in g.EuTree:
+    add_edge(x[0], x[1], x[2])
 
-# ETP = EulerTour(1, g.vertices)
-# Visited = []
+ETP = EulerTour(1, g.vertices)
+Visited = []
 
-# for x in ETP:
-#     if x not in Visited:
-#         Visited.append(x)
-
-
-# def costEval(node):
-#     cost = 0
-#     for x in range(len(node)):
-#         From = node[x]
-#         if x == 99:
-#             To = node[0]
-#         else:
-#             To = node[x+1]
-#         cost += nD[From][To]
-#     return cost
+for x in ETP:
+    if x not in Visited:
+        Visited.append(x)
 
 
-# Visited = np.array(Visited)
-# end = time.time()
-# curr = Visited
-# try:
-#     while (end - start < 100):
+def costEval(node):
+    cost = 0
+    for x in range(len(node)):
+        From = node[x]
+        if x == 99:
+            To = node[0]
+        else:
+            To = node[x+1]
+        cost += nD[From][To]
+    return cost
 
-#         p = random.random()
-#         p=0.6
-#         neighbours = copy.deepcopy(curr)
-#         x = np.random.randint(0, 100)
-#         y = np.random.randint(0, 100)
-#         m = min(x, y)
-#         M = max(x, y)
-#         if p <= 0.5:  # swap
-#             temp = neighbours[x]
-#             neighbours[x] = neighbours[y]
-#             neighbours[y] = temp
-#         else:  # insert
-#             temp = neighbours[m]
-#             for i in range(m, M):
-#                 neighbours[i] = neighbours[i+1]
-#             neighbours[M] = temp
-#         if costEval(neighbours) < costEval(curr):
-#             curr = neighbours
-#             print(costEval(curr))
-#         end = time.time()
-# except KeyboardInterrupt:
-#     print(costEval(curr), end-start)
-# # print(costEval(Visited))
+
+Visited = np.array(Visited)
+end = time.time()
+curr = Visited
+try:
+    while (end - start < 200):
+
+        p = random.random()
+        p=0.4
+        neighbours = copy.deepcopy(curr)
+        x = np.random.randint(0, g.vertices)
+        y = np.random.randint(0, g.vertices)
+        z = np.random.randint(0, g.vertices)
+        m = min(x, y,z)
+        M = max(x, y,z)
+        if p <= 0.5:  # swap
+            temp = neighbours[x]
+            neighbours[x] = neighbours[y]
+            neighbours[y] = neighbours[z]
+            neighbours[z] = temp
+        else:  # insert
+            temp = neighbours[m]
+            for i in range(m, M):
+                neighbours[i] = neighbours[i+1]
+            neighbours[M] = temp
+        if costEval(neighbours) < costEval(curr):
+            curr = neighbours
+            print(costEval(curr))
+        end = time.time()
+except KeyboardInterrupt:
+    print(costEval(curr), end-start)
+print(*curr)
+# print(costEval(Visited))
